ee4a8d3826e545993b1750f74bc884b2c8635feb
Давыдов Александр <madyson123@yandex.ru>
2023-12-25 21:08:11 +0300

    fixed size difference

--- src/components/common/ItemCard/itemCard.jsx
+++ src/components/common/ItemCard/itemCard.jsx
@@ -4,19 +4,19 @@ import ItemImages from '../ItemImages/itemImages';
 
 function ItemCard({ item }) {
   return (
-    <div className="card">
+    <div className="card" style={{ height:'100%' }}>
       <ItemImages
         images={item.images}
         title={item.title}
       />
-      <div className="card-body">
+      <div className="card-body" style={{ display: 'flex', flexDirection: 'column' }}>
         <p className="card-text">{item.title}</p>
         <p className="card-text">
           {item.price.toLocaleString()}
           {' '}
           руб.
         </p>
-        <NavLink to={`/products/${item.id}`} className="btn btn-outline-primary">Заказать</NavLink>
+        <NavLink to={`/products/${item.id}`} className="btn btn-outline-primary" style={{  marginTop: 'auto' }}>Заказать</NavLink>
       </div>
     </div>
   );
