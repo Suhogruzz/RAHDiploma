d1f1a256237ff8ae0a29de6957d600664d0871c7
Давыдов Александр <madyson123@yandex.ru>
2023-12-30 20:45:34 +0300

    fixed search

--- src/components/header/SearchBar/searchBar.jsx
+++ src/components/header/SearchBar/searchBar.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import './searchBar.css';
 
 function SearchBar({ searchUrl }) {
   const [show, setShow] = useState(false);
+  const [search, setSearch] = useState();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSearch (e.target.value);
+  }
+
 
   return (
     <div className="header-controls-pic header-controls-search">
       <form
-        action={searchUrl}
+        onSubmit={(e) => {
+          e.preventDefault();
+          navigate(searchUrl + '?q=' + search);
+        }}
+        method='GET'
         className={`header-controls-search-form form-inline ${show ? 'control-show' : 'invisible'}`}
       >
         <input
           className="form-control control-show"
           placeholder="Поиск"
           name="q"
+          value={search}
+          onChange={handleChange}
         />
       </form>
       <img
